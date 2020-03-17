ab84ce509428d3f275131764cbbb28f0fac193a3
arison <liu_sl2005@126.com>
2020-03-17 11:54:49 +0800

    lsl

--- NET2CommonHelper/auto-pack.bat
+++
@@ -1,3 +0,0 @@
-nuget setapikey oy2ah26eu43kiyahpjxqdq7g56bug5le5oh5vouih3vztu
-nuget pack NET2CommonHelper.csproj -Build -Properties owners="arison";description="just only for .net framework 2.0 programs!if your programs run on .net framework runtimes,you can use it ";id="NET2CommonHelper" -Version 1.0.0
-nuget push NET2CommonHelper.1.0.0.nupkg -Source https://www.nuget.org/api/v2/package
